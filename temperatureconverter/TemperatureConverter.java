81284f42f03824817478730c1e3fc9ae953bdb1d
Kmorris1370 <kmorris1370@gmail.com>
2025-11-15 19:06:44 -0600

    adding program
    Adding temperature converter program

---
+++ temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,16 @@
+package temperatureconverter;
+
+/**
+ * @author Kaitlyn Morris
+ * CS 491 Homework 2
+ * 10/08/25 
+ */
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI g = new GUI();
+        g.setVisible(true);
+    }
+    
+}
